aeaf049ddf9979add1f379441e7b8ef275789f19
Krishna3267 <krishnashah2607@gmail.com>
2022-01-24 00:45:09 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,134 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Card from './components/Card';
+
+const cardImages = [
+  {"src" : "/img/diamond.png", matched: false},
+  {"src" : "/img/hat.png", matched: false},
+  {"src" : "/img/joker.png", matched: false},
+  {"src" : "/img/pen.png", matched: false},
+  {"src" : "/img/sword.png", matched: false},
+  {"src" : "/img/wand.png", matched: false}
+] 
 
 function App() {
+
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+
+  //access the selected choices
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+
+  const [disabled, setDisabled] = useState(false);
+
+  //duplicate each card once
+  //randomize the order of cards in array using sort method
+  //give a random id to each card and use the id to output it
+
+  const shuffleCards = () => {
+
+    const shuffledCards = [...cardImages, ...cardImages].sort(
+      () => Math.random() - 0.5
+      ).map(
+        (card) => ({...card, id: Math.random()})
+      )
+      setChoiceTwo(null)
+      setChoiceTwo(null)
+      setCards(shuffledCards)
+      setTurns(0);
+      console.log("new game pressed");
+  }
+
+// console.log(cards, turns);
+
+//to setchoice one and two // take it a link between Card.js and App.js
+const handleChoice = (card) => {
+  // console.log(card);
+  choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+
+}
+
+
+// useEffect(() => {
+  
+//   const fireWhenTwoChanges = () => {
+//     console.log("yea two changed")
+//     if(choiceOne.src === choiceTwo.src){
+//       console.log("yes");
+//     }else{
+//       console.log("no");
+//     }
+    
+  
+//   }
+
+//   if(choiceTwo){
+//     fireWhenTwoChanges();
+//     resetTurn();
+//   }
+    
+
+// }, [choiceTwo]);
+
+useEffect(() => {
+  
+  if(choiceOne && choiceTwo){
+    setDisabled(true)
+    if(choiceOne.src === choiceTwo.src){
+      // console.log("yeah");
+      setCards( prevCards => {
+        return prevCards.map(card => {
+          if(card.src === choiceOne.src){
+            return {...card, matched: true}
+          }else{
+            return card
+          }
+        })
+      })
+      resetTurn()
+    }
+    else{
+      // console.log("No")
+      setTimeout(() => {
+        resetTurn()
+      }, 1000);
+
+    }
+  }
+}, [choiceOne, choiceTwo]);
+
+// console.log(cards);  
+
+const resetTurn = () => {
+  setDisabled(false)
+  setChoiceOne(null)
+  setChoiceTwo(null)
+  setTurns(prevTurns => prevTurns + 1)
+}
+
+useEffect(() => {
+  shuffleCards()
+}, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory Magic</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      
+      <div className="card-grid">
+        {cards.map(card => (
+          <Card 
+            handleChoice={handleChoice}
+            key={card.id} 
+            card={card}
+            flipped = {card === choiceOne || card=== choiceTwo || card.matched}
+            disabled={disabled} />
+        ))}
+      </div>
+      <p>Turns : {turns}</p>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import './SingleCard.css';
+
+const Card = ({card, handleChoice, flipped, disabled}) => {
+
+    const handleClick = () => {
+        if(!disabled){
+            handleChoice(card)
+        }
+    }
+    return ( 
+    <div className="card">
+        <div className={flipped ? "flipped" : ""}>
+            <img src={card.src} className='front' alt='card front' />
+            <img 
+            onClick={handleClick} 
+            src="/img/cover.png" 
+            className='back' 
+            alt="card cover" />
+        </div>
+    </div>
+     );
+}
+ 
+export default Card;
